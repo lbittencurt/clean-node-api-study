395b2e97e8a0d025e786264876d2b217c82eb8c5
Lucas Bittencurt <bittencurt.lucas@outlook.com>
2020-12-17 00:19:18 -0300

    chore: add jest configs and scripts

---
+++ jest-unit-config.js
@@ -0,0 +1,21 @@
+// const config = require('./jest.config.ts')
+// config.testMatch = ['**/*.spec.ts']
+module.export = {
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/*.spec.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**',
+    '!**/test/**'
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1'
+  }
+}
